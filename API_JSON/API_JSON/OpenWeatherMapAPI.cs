0e1551c4f957a718c4522c0468a51104daf8d845
Brooks Stockley <bstockley26@gmail.com>
2024-06-10 11:40:15 -0400

    finished

---
+++ API_JSON/API_JSON/OpenWeatherMapAPI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace API_JSON
+{
+	public class OpenWeatherMapAPI
+	{
+        public static void GetTemp()
+        {
+            var appsettingsText = File.ReadAllText("appsettings.json");
+
+            var apiKey = JObject.Parse(appsettingsText)["apiKey"].ToString();
+
+
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip=28173,&appid={apiKey}&units=Imperial";
+
+            var client = new HttpClient();
+
+            var jsonText = client.GetStringAsync(weatherURL).Result;
+
+            var weatherObj = JObject.Parse(jsonText);
+
+            Console.WriteLine($"Ron says the temperature in Charlotte is {weatherObj["main"]["temp"]}degrees");
+
+        }
+    }
+}
+
---
+++ API_JSON/API_JSON/RonVSKanyeAPI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace API_JSON
+{
+	public class RonVSKanyeAPI
+	{
+        public static void Convo()
+        {
+            var client = new HttpClient();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye says \n {GetKanyeQuote(client)}\n");
+                Console.WriteLine();
+                Console.WriteLine($"Ron says \n {GetRonQuote(client)}\n");
+
+            }
+        }
+        private static string GetRonQuote(HttpClient client)
+        {
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = ronResponse.Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+            return ronQuote;
+        }
+
+        private static string GetKanyeQuote(HttpClient client)
+        {
+            var jsonText = client.GetStringAsync("https://api.kanye.rest/").Result;
+
+            var quote = JObject.Parse(jsonText).GetValue("quote").ToString();
+            return quote;
+        }
+
+    }
+}
+
